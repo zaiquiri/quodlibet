6c6e29cb6abd7bab9459c734fafe2a921f8ee927
zaiquiri <zachary.lindberg@gmail.com>
2014-10-19 16:27:32 -0400

    Created tests directory

--- test-htmlParser.js
+++
@@ -1,7 +0,0 @@
-var HtmlParser = require('./htmlParser.js')
-var fs = require('fs')
-
-var htmlText = fs.readFileSync('./testPage.html', "utf8")
-var nodeTree = HtmlParser.createNodeTreeFor(htmlText)
-
-console.log(nodeTree)
