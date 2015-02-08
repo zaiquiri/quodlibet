04fbb0aafae25069d49d3cfb28b570ff31a0620e
zaiquiri <zachary.lindberg@gmail.com>
2015-02-08 14:36:23 -0500

    Added grunt watch to run jasmine tests on file changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,10 +16,17 @@ module.exports = function(grunt) {
         }
       },
       all: ['specs/']
+    },
+    watch: {
+      test: {
+        files: ['src/*'],
+        tasks: ['jasmine_node']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-jasmine-node');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default', ['jasmine_node']);
 
 };
