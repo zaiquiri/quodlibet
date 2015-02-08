ec7838c9755eee3f6960e748ce8551845a24ce0e
zaiquiri <zachary.lindberg@gmail.com>
2015-02-08 02:02:11 -0500

    updated functional test to work with new dir structure

--- tests/test-htmlParser.js
+++ tests/test-htmlParser.js
@@ -1,4 +1,4 @@
-var HtmlParser = require('../htmlParser.js')
+var HtmlParser = require('../src/htmlParser.js')
 var fs = require('fs')
 
 var htmlText = fs.readFileSync('./tests/testPage.html', "utf8")
