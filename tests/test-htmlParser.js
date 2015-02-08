804e0d2c89fc022007657f477c9e5278a8cad282
zaiquiri <zachary.lindberg@gmail.com>
2015-02-08 13:33:10 -0500

    Converted 'manual test' to jasmine test

--- tests/test-htmlParser.js
+++
@@ -1,7 +0,0 @@
-var HtmlParser = require('../src/htmlParser.js')
-var fs = require('fs')
-
-var htmlText = fs.readFileSync('./tests/testPage.html', "utf8")
-var nodeTree = HtmlParser.createNodeTreeFor(htmlText)
-
-console.log(nodeTree)
