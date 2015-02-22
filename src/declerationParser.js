c78f3d0b7796517a29b7f59e2ad72259f6f457b2
zaiquiri <zachary.lindberg@gmail.com>
2015-02-22 16:57:53 -0500

    refactored declerationParser

--- src/declerationParser.js
+++ src/declerationParser.js
@@ -1,18 +1,31 @@
 DeclerationParser.prototype.getDeclerationsFrom = function(rule){
   rule = new this.TextStream(rule);
-  var endOfRule = function(previous, next){ return '}' === next; }
-  rule.popUntil(function(previous){ return previous === '{' });
-  var declerationList = rule.popUntil(endOfRule);
-  var declerationTokens = declerationList.split(';')
-    .filter(function(z){ return !(/^\s*$/.test(z))});
-  var declerations = declerationTokens.map(function(decleration){
-    pair = decleration.split(':').filter(function(z){ return !(/^\s*$/.test(z))});
-    console.log(pair)
-    pair = pair.map(function(z){return z.trim();});
-    return { name:pair[0], value:pair[1] };
-  })
-  return declerations
 
+  this.fastFowardToDeclerations(rule);
+  var declerationList = rule.popUntil(this.endOfRule);
+
+  var declerations = declerationList.split(';')
+    .filter(this.isNotWhiteSpace)
+    .map(this.extractToPair.bind(this));
+
+  return declerations;
+};
+
+DeclerationParser.prototype.isNotWhiteSpace = function(z){
+  return !(/^\s*$/.test(z));
+};
+
+DeclerationParser.prototype.endOfRule = function(previous, next){ 
+  return '}' === next;
+};
+
+DeclerationParser.prototype.fastFowardToDeclerations = function(rule){ 
+  rule.popUntil(function(previous){ return previous === '{'; });
+};
+
+DeclerationParser.prototype.extractToPair = function(decleration){
+  var pair = decleration.split(':').map(function(z){return z.trim();});
+  return { name:pair[0], value:pair[1] };
 };
 
 module.exports = DeclerationParser;
